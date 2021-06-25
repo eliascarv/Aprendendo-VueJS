3e5e072acc64f22ecdad818bf920eb57fd30332a
Elias Carvalho <eliascarv1998@gmail.com>
2021-06-25 16:03:04 -0300

    Atrbutos Dinamicos

---
+++ src/components/Imagem.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="src" :alt="descricao">
+</template>
+
+<script>
+export default {
+  name: 'Imagem',
+  data() {
+    return {
+      src: '/img/avatar.png',
+      descricao: 'Avatar'
+    }
+  }
+}
+</script>
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,18 +3,30 @@
     <p v-if="esta_trab">Estou trabalhando como {{ profissao }}.</p>
     <p v-else>Estou a procura de uma oportunidade.</p>
     <p v-show="mostrar_email">Meu email é: {{ email }}</p>
+    <p>O link do meu perfil no github é o seguinte: <a v-bind:href="github">eliascarv</a></p>
+    <imagem />
   </div>
 </template>
 
+// Atributos Dinamicos: eles podem conter código vue. 
+// É possivel tornar qualquer atributo de uma tag html passivel de receber código vue ou js
+// Isso é possivel adicionando v-bind: antes do nome do atributo
+// Como forma abreviada também é possivel simplismente adicionar : antes do nome do atributo
 <script>
+import Imagem from './Imagem.vue'
+
 export default {
   name: 'Info',
+  components: {
+    Imagem
+  },
   data() {
     return {
       profissao: 'Programador',
       email: 'eliascarv@email.com',
       esta_trab: true,
-      mostrar_email: true
+      mostrar_email: true,
+      github: 'https://github.com/eliascarv'
     }
   }
 }
