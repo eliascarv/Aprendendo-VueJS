57c5633062c0614bcd0cc1db03453d36ebc71827
Elias Carvalho <eliascarv1998@gmail.com>
2021-06-25 08:09:17 -0300

    Primeiro Comp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <primeiro-comp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PrimeiroComp from './components/PrimeiroComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PrimeiroComp
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
