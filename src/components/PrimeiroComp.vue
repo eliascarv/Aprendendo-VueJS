b152a82fdaa759a6b0770f077621199f5dba6276
Elias Carvalho <eliascarv1998@gmail.com>
2021-06-25 09:01:49 -0300

    Explicações

--- src/components/PrimeiroComp.vue
+++ src/components/PrimeiroComp.vue
@@ -1,3 +1,4 @@
+// Em tempalte, se ouver mais de uma tag elas devem ficar dentro de uma div
 <template>
   <div>
     <h1>Hello Vue!</h1>
@@ -5,6 +6,13 @@
   </div>
 </template>
 
+// Entre algumas das propriedades mais importantes de um componente está o name
+// O name define o nome do componete, o padrão é usar o estilo camelo
+// Porém quando o componete é usado no html ele sofre algumas alterações
+// O padão camelo é alterado para o seguite padrão: PrimeiroComp = <primeiro-comp />
+// Outra propriedadde muito importante de um componte é o método data()
+// Nessa proprieadade você irá retornar um objeto que irá conter as poroprieades e os métodos do componte
+// Essas propriedades e métodos podem ser acessdas no template do comp usando chaves duplas {{}}
 <script>
 export default {
   name: "PrimeiroComp",
