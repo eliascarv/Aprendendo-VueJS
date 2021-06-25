f29bf5ef80c5bedb9a70bd734376d9320d16ac56
Elias Carvalho <eliascarv1998@gmail.com>
2021-06-25 08:09:39 -0300

    Primeiro Comp

---
+++ src/components/PrimeiroComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello Vue!</h1>
+    <p>Meu nome é {{ nome }} e eu tenho {{ idade }} anos. </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComp",
+  data() {
+    return {
+      nome: "Elias",
+      idade: 22
+    }
+  }
+}
+</script>
