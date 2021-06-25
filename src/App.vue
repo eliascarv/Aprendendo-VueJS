ac3debf6d0a00c31f991a45c43d5f68550d7ef30
Elias Carvalho <eliascarv1998@gmail.com>
2021-06-25 13:47:28 -0300

    Diretivas v-if, v-else, v-show

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <primeiro-comp />
   <life-cycle />
+  <info />
 </template>
 
 <script>
 import PrimeiroComp from './components/PrimeiroComp.vue'
 import LifeCycle from './components/LifeCycle.vue'
+import Info from './components/Info.vue'
 
 export default {
   name: 'App',
   components: {
     PrimeiroComp,
-    LifeCycle
+    LifeCycle,
+    Info
   }
 }
 </script>
---
+++ src/components/Info.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-if="esta_trab">Estou trabalhando como {{ profissao }}.</p>
+    <p v-else>Estou a procura de uma oportunidade.</p>
+    <p v-show="mostrar_email">Meu email é: {{ email }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Info',
+  data() {
+    return {
+      profissao: 'Programador',
+      email: 'eliascarv@email.com',
+      esta_trab: true,
+      mostrar_email: true
+    }
+  }
+}
+</script>
